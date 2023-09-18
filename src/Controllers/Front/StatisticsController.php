7ba0ea1762cc6245ae3b94ba3d97acbfd2907a7b
Aphly <121099327@qq.com>
2023-09-18 15:14:47 +0800

    update

--- src/Controllers/Front/StatisticsController.php
+++ src/Controllers/Front/StatisticsController.php
@@ -17,19 +17,23 @@ class StatisticsController extends Controller
             $input['ipv4'] = $request->ip();
             $url = parse_url($input['url']);
             if($input['appid'] && $url){
-                $StatisticsSite = StatisticsSite::where('appid',$input['appid'])->where('status',1)->firstOrError();
-                if($url['host']==$StatisticsSite->host){
-                    $input['url'] = '/'.basename($input['url']);
-                    $input['site_id'] =$StatisticsSite->id;
-                    $info = Statistics::where('site_id',$StatisticsSite->id)->where('ipv4',$input['ipv4'])->where('url',$input['url'])->first();
-                    if(!empty($info) && $info->created_at->isToday()){
-                        $info->increment('view');
-                    }else{
-                        $ip_int = ip2long($input['ipv4']);
-                        $ipv4_lib = StatisticsIpv4::where('ip_start_int','<=',$ip_int)->where('ip_end_int','>=',$ip_int)->first();
-                        $input['country_iso'] = !empty($ipv4_lib)?$ipv4_lib->country_iso:'';
-                        Statistics::create($input);
+                try{
+                    $StatisticsSite = StatisticsSite::where('appid',$input['appid'])->where('status',1)->firstOrError();
+                    if($url['host']==$StatisticsSite->host){
+                        $input['url'] = '/'.basename($input['url']);
+                        $input['site_id'] =$StatisticsSite->id;
+                        $info = Statistics::where('site_id',$StatisticsSite->id)->where('ipv4',$input['ipv4'])->where('url',$input['url'])->first();
+                        if(!empty($info) && $info->created_at->isToday()){
+                            $info->increment('view');
+                        }else{
+                            $ip_int = ip2long($input['ipv4']);
+                            $ipv4_lib = StatisticsIpv4::where('ip_start_int','<=',$ip_int)->where('ip_end_int','>=',$ip_int)->first();
+                            $input['country_iso'] = !empty($ipv4_lib)?$ipv4_lib->country_iso:'';
+                            Statistics::create($input);
+                        }
                     }
+                }catch (\Exception $e){
+                    throw new ApiException(['code'=>1,'msg'=>'fail']);
                 }
             }
         }
